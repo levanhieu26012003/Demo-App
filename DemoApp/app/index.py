451a3a84e9ddb9b1985410d61244d5efc8f23351
Hieues <levanhieu26012003@gmail.com>
2023-10-25 18:39:20 +0700

    up

--- DemoApp/app/index.py
+++ DemoApp/app/index.py
@@ -1,10 +1,15 @@
-from flask import Flask
-
+from flask import Flask, render_template,request
+import dao
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return render_template('index.html')
+
+    kw = request.args.get("kw")
+
+    navbar =  dao.load_nav()
+    categories = dao.load_categories(kw)
+    return render_template('index.html', nav = navbar, categories = categories)
 
 if __name__ == '__main__' :
     app.run(debug=True)
